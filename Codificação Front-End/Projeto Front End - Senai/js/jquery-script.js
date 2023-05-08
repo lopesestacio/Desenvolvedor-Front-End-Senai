d99eaececf82611116651935e8500ee207b99cf2
Felipe Guimarães <85319756+lopesestacio@users.noreply.github.com>
2023-05-08 01:01:42 -0300

    Add files projeto front end

---
+++ Codificação Front-End/Projeto Front End - Senai/js/jquery-script.js
@@ -0,0 +1,10 @@
+//efeito de esconder formulário de cadastro
+$(document).ready(function(){
+
+    $("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slideToggle("slow");
+        
+        
+    });
+});
